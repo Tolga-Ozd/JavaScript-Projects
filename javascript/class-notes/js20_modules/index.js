763c27b6590da644ce59c793f9f3d23104831050
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-06-21 20:25:24 +0300

    push

--- javascript/class-notes/js20_modules/index.js
+++ javascript/class-notes/js20_modules/index.js
@@ -1,1 +1,25 @@
 console.log("JS Modules is running")
+
+//! module kullanımında this objesi window nesnesini göstermez
+
+console.log(window);
+console.log(this)    //undefined
+
+import {CALIŞMA_SAATI , maas } from "./mymodule.js"
+import {arttir , azalt } from "./mymodule.js"
+
+console.log(CALIŞMA_SAATI);
+console.log(maas);
+
+console.log(arttir(5));
+console.log(azalt(5));
+
+//!Export Default (süslü parantez kulanılmaz)
+
+import mesajVer from "./mymodule.js"
+
+mesajVer()
+
+//! bazen farklı modüllerden aynı elemanı import etmek gerekebilir. Bunun için as ile isim değişikliği yapmak mantıklı olacaktır.
+
+import {arttir as inc, azalt as dec } from "./mymodule.js"
--- javascript/class-notes/js20_modules/mymodule.js
+++ javascript/class-notes/js20_modules/mymodule.js
@@ -1,1 +1,19 @@
 console.log("My Module Loaded")
+
+//!Named export
+
+export const CALIŞMA_SAATI = 40
+export let maas = 20000
+
+let sayac = 0
+export const arttir = (miktar) => {
+    sayac += miktar 
+    return sayac
+}
+
+export const azalt = (miktar) =>{
+    sayac -= miktar
+    return sayac
+} 
+
+//!Export Default (her modulde sadece birtane olur)
