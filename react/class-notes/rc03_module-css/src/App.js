677a4127c5a008522f19b9a270c3196139c9a9ca
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-07 23:53:12 +0300

    push

--- react/class-notes/rc03_module-css/src/App.js
+++ react/class-notes/rc03_module-css/src/App.js
@@ -1,7 +1,25 @@
+import Card from "./components/card/Card"
+import data from "./data"
+
 function App() {
+
+  console.log(data)
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div >
+      <h1>LANGUAGES</h1>
+
+      { data.map(( {id , language , img , btnName }) => (
+        <Card key ={id} lang= {language} img={img} btn = {btnName} />
+      ))}
+
+      {/* Birden fazla statement kullanılacaksa o zaman fonsksiyonun süslü ve return keywordü kullanması gerekir */}
+
+      {/* < Card
+       id = {data.id} 
+       lang = {data.language} 
+       img = {data.img} 
+       btn = {data.btnName} /> */}
+
     </div>
   )
 }
