65d76082934a76df00ebb44abc120becd57280cb
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-13 19:50:20 +0300

    Appoinment-App

---
+++ react/class-notes/rc08_appoinment-app/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css"
+import Home from "./pages/Home"
+import {AiFillDelete} from "react-icons/ai";
+
+const App = () => {
+  return (
+    <div>
+    <Home />
+
+    <AiFillDelete />
+
+</div>
+  ) 
+}
+
+export default App
