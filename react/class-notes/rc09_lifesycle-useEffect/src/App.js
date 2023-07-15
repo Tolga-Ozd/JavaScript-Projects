3af1a34dd403e600b27bff082417298dc50464a9
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-15 12:56:33 +0300

    Fetch-Api

---
+++ react/class-notes/rc09_lifesycle-useEffect/src/App.js
@@ -0,0 +1,27 @@
+
+// import LifeCycleMethods from "./components/LifeCycleMethods"
+// import UseEffectHook from "./components/UseEffectHook"
+// import { useState } from "react"
+import User from "./components/User"
+
+function App() {
+  // const [visible, setVisible] = useState()
+  return (
+    <div className="container text-center mt-4">
+
+      {/* <button className="btn btn-danger" onClick={()=>setVisible(!visible)}>
+          Show/Hide
+      </button>
+
+      {/* {visible && <LifeCycleMethods />} */}
+      {/* {visible && < UseEffectHook />} */} 
+
+
+    <User />
+
+
+    </div>
+  )
+}
+
+export default App
