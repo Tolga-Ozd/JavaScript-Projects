11cb629d43e25ae81c95fb7841692c7887d2d193
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-17 12:42:21 +0300

    added-todo

--- react/class-notes/rc10_React-ToDoApp/src/App.js
+++ react/class-notes/rc10_React-ToDoApp/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header"
 import TodoList from "./components/TodoList"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
--- react/class-notes/rc10_React-ToDoApp/src/components/Header.jsx
+++ react/class-notes/rc10_React-ToDoApp/src/components/Header.jsx
@@ -1,13 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {as as as2017} from as;
 
-const Header = () => {
+const Header = ({setTodos , todos}) => {
+
+    const [task , setTask] = useState({
+        id: "" ,
+        text: "" ,
+    })
+
+    const handleSubmit = (e)=> {
+        e.preventDefault()
+        setTodos([...todos , task])
+        setTask({
+            id:"",
+            text:""
+        })
+    }
+
+    const handleInput = (e) =>{
+        setTask({
+            id: as2017(),
+            text: e.target.value,
+            cheched:false
+        })
+    }
+
   return (
-    <div>
-      <h1>Todo App</h1>
-      <div>
-        <form action="#">
-            <input type="" />
-            <button>Add</button>
+    <div className='container text-center mt-5'>
+      <h1 className='text-danger'>Todo App</h1>
+      <div className='row justify-content-center mt-3'>
+        <form onSubmit={handleSubmit} action="#" className='d-flex w-50 '>
+            <input onChange={handleInput} type="text"  className='form-control' value={task.text}  required/>
+            <button type='submit' className='btn btn-primary ms-1'>Add</button>
         </form>
       </div>
     </div>
