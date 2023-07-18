a1f4e689e6b3bac350f8c0b8755007c1759bcb3d
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-18 19:12:51 +0300

    style added

--- react/class-notes/react.state/src/App.js
+++ react/class-notes/react.state/src/App.js
@@ -1,33 +1,27 @@
-import Course from "./Course"
-import {useState} from "react"
+import Course from './Course';
+import { useState } from 'react';
+import "./App.css"
 
 function getRandomCourse() {
   const courseArray = ["Bootstrap" ,"Web" , "Developer" , "React2"];
-  return courseArray[Math.floor(Math.random()*courseArray.length)];
+  return courseArray[Math.floor(Math.random() * courseArray.length)];
 }
 
 function App() {
-  const [courses , setCourses] = useState([]);
+  const [courses, setCourses] = useState([]);
 
-  const handleClick = () =>{
-       setCourses([...courses , getRandomCourse()]);
+  const handleClick = () => {
+    setCourses([...courses, getRandomCourse()]);
   };
-
+  const courseList = courses.map((course, index) => {
+    return <Course key={index} courseName={course} />;
+  });
   return (
     <div className="App">
-
-      <button onClick={handleClick}>
-        Add Course
-      </button>
-
-      {courses.map((course ,index)=> {
-        return 
-        <Course key={index} courseName = {course} />;
-         
-      })}
-
+      <button className='appButton' onClick={handleClick}>Add Course</button>
+      <div className='courseList'>{courseList}</div>
     </div>
   );
 }
 
-export default App
+export default App;
--- react/class-notes/react.state/src/Course.js
+++ react/class-notes/react.state/src/Course.js
@@ -1,15 +1,17 @@
-import Bootstrap from "./images/bootstrap-5.png"
-import Web from "./images/comple-web.jpg"
+import Bootstrap from "./images/bootstrap5...png"
+import Web from "./images/compleweb.jpg"
 import Developer from "./images/developer.jpg"
 import React2 from "./images/react2.png"
+import "./Course.css"
 
+const courseMap = {Bootstrap , Web , Developer , React2}
 
 function Course({courseName}) {
-    console.log(Bootstrap)
-    console.log(courseName)
+    console.log(Bootstrap);
+    console.log(courseName);
     return (
-        <div>
-            <img src={Bootstrap} alt="" />
+        <div className="courseDiv">
+            <img className="course" src={courseMap[courseName]} alt="course" />
         </div>
     );
 }
