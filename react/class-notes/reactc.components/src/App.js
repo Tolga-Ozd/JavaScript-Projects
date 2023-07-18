9b58fa98d7deb4126e042c4fee2492962fe67e7d
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-18 09:26:24 +0300

    props-fixed

--- react/class-notes/reactc.components/src/App.js
+++ react/class-notes/reactc.components/src/App.js
@@ -1,14 +1,23 @@
+import React from "react"
 import Course from "./Course"
 
 function App() {
   return (
     <div className="App">
       <Course
-      title = "Angular" 
-      description = "lorem10"/>
-      <Course />
-      <Course />
-      <Course /> 
+      title = "Bootstrap 5"
+      image = {Bootstrap} 
+      description = "Bootstrap is one of the most used open source design tools, it is a free CSS framework where we can make designs suitable for the theme you want in proportion to the device size used for your desktop computers, mobile phone or tablet. We continued our designs with Bootstrap 4 Version for a long time, but Bootstrap took its share in the constantly renewed and updated information and technology field, and the expected new version update came and Bootstrap V5 was ready to use."/>
+      <Course title = "React"
+      image = {React} 
+      description="React, single-page sayfalar ve mobil uygulamalar geliştirmek için kullanılır.  React, hızlı ve interaktif kullanıcı arayüzü geliştirmeyi kolaylaştırır. Örneğin Instagram arayüzünde karşımıza çıkan arama kutusu, logo ve mesaj butonu gibi görsellerin oluşturulmasını kolaylaştırır.  Arayüzdeki tüm işlemleri kolay, maliyetsiz ve yüksek performanslı olarak yapar. Bu nedenle verilerin ve özelliklerin çok hızlı değiştiği uygulamalara kullanılır."/>
+      <Course 
+      title = "Comple Web "
+      image = {comple web} 
+      description="Complete Application Web Development training has a plan that will make the whole process easier for you. Normally, if you did not have such a planned resource, this process is 2-3 years, but we can greatly reduce this process by just following the course and without needing any other resources. ** The recommended duration of study for the course is 6-8 months. This process, which is completed in less time, may not be very beneficial."/>
+      <Course title="Frontend Developer"
+      image = {developer} 
+      description="While the FrontEnd developer includes content about visual components, he is also responsible for its application in interactions that will increase usability. The front-end developer should have knowledge about testing and debugging, if any. In addition, he should have detailed knowledge and understanding of front-end programming languages."/> 
     </div>
   )
 }
--- react/class-notes/reactc.components/src/Course.js
+++ react/class-notes/reactc.components/src/Course.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-const Course = ({title ,description}) => {
+const Course = ({image ,title ,description}) => {
     // props üstteki gibi tanımlanacağı gibi alttaki gibi de tanımlanabilir
     // const {title , description} = props;
 
   return (
     <div>
         <div>{title}</div>
+        <div>{image}</div>
         <div>{description}</div>
     </div>
   )
