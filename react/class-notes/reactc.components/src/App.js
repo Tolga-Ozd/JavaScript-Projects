b5eb80507531e0e8c3d64c304ade0de2c6b59a5b
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-18 13:57:18 +0300

    css added

--- react/class-notes/reactc.components/src/App.js
+++ react/class-notes/reactc.components/src/App.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "bulma/css/bulma.css"
+import "./App.css"
 import Course from "./Course"
 import Bootstrap from "./images/bootstrap-5.png"
 import Reactkurs from "./images/react2.png"
@@ -28,28 +29,28 @@ function App() {
           <Course
         image = {Bootstrap} 
         title = "Bootstrap 5"
-        description = "Bootstrap is one of the most used open source design tools, it is a free CSS framework where we can make designs suitable for the theme you want in proportion to the device size used for your desktop computers, mobile phone or tablet. We continued our designs with Bootstrap 4 Version for a long time, but Bootstrap took its share in the constantly renewed and updated information and technology field, and the expected new version update came and Bootstrap V5 was ready to use."/>
+        description = "Bootstrap is one of the most used open source design tools, it is a free CSS framework where we can make designs suitable for the theme you want in proportion to the device size used for your desktop computers, mobile phone or tablet. "/>
         </div>
 
         <div className="column">
           <Course 
           image = {Reactkurs} 
           title = "React"
-          description="React, single-page sayfalar ve mobil uygulamalar geliştirmek için kullanılır.  React, hızlı ve interaktif kullanıcı arayüzü geliştirmeyi kolaylaştırır. Örneğin Instagram arayüzünde karşımıza çıkan arama kutusu, logo ve mesaj butonu gibi görsellerin oluşturulmasını kolaylaştırır.  Arayüzdeki tüm işlemleri kolay, maliyetsiz ve yüksek performanslı olarak yapar. Bu nedenle verilerin ve özelliklerin çok hızlı değiştiği uygulamalara kullanılır."/>
+          description="React is used to develop single-page pages and mobile applications. React makes it easy to develop a fast and interactive user interface. For example, it facilitates the creation of images such as the search box, logo and message button in the Instagram interface."/>
         </div>
 
         <div className="column">
             <Course 
           image = {Compleweb} 
           title = "Comple Web "
-          description="Complete Application Web Development training has a plan that will make the whole process easier for you. Normally, if you did not have such a planned resource, this process is 2-3 years, but we can greatly reduce this process by just following the course and without needing any other resources. ** The recommended duration of study for the course is 6-8 months. This process, which is completed in less time, may not be very beneficial." />
+          description="Complete Application Web Development training has a plan that will make the whole process easier for you. Normally, if you did not have such a planned resource, this process is 2-3 years.." />
         </div>
 
         <div className="column">    
           <Course 
           image = {developer} 
           title="Frontend Developer"
-          description="While the FrontEnd developer includes content about visual components, he is also responsible for its application in interactions that will increase usability. The front-end developer should have knowledge about testing and debugging, if any. In addition, he should have detailed knowledge and understanding of front-end programming languages."/> 
+          description="While the FrontEnd developer includes content about visual components, he is also responsible for its application in interactions that will increase usability. The front-end developer should have knowledge about testing and debugging, if any."/> 
         </div>
       </div>
       </section>
