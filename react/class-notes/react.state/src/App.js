3cb0f071ec3e7e5c2c3497eba7ca8240acf0de20
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-18 21:48:03 +0300

    random background added

--- react/class-notes/react.state/src/App.js
+++ react/class-notes/react.state/src/App.js
@@ -7,21 +7,32 @@ function getRandomCourse() {
   return courseArray[Math.floor(Math.random() * courseArray.length)];
 }
 
-function App() {
-  const [courses, setCourses] = useState([]);
-
-  const handleClick = () => {
-    setCourses([...courses, getRandomCourse()]);
-  };
-  const courseList = courses.map((course, index) => {
-    return <Course key={index} courseName={course} />;
-  });
-  return (
-    <div className="App">
-      <button className='appButton' onClick={handleClick}>Add Course</button>
-      <div className='courseList'>{courseList}</div>
-    </div>
-  );
+function getRandomColor() {
+  const colors = ["red", "blue", "green", "yellow", "purple", "orange"];
+  return colors[Math.floor(Math.random() * colors.length)];
 }
+
+
+  function App() {
+    const [courses, setCourses] = useState([]);
+    const [backgroundColor, setBackgroundColor] = useState("white"); // Başlangıçta beyaz arka plan
+  
+    const handleClick = () => {
+      setCourses([...courses, getRandomCourse()]);
+      setBackgroundColor(getRandomColor()); // Rastgele bir arka plan rengi belirle
+    };
+  
+    const courseList = courses.map((course, index) => {
+      return <Course key={index} courseName={course} />;
+    });
+  
+    return (
+      <div className="App" style={{ backgroundColor }}>
+        <button className='appButton' onClick={handleClick}>Add Course</button>
+        <div className='courseList'>{courseList}</div>
+      </div>
+    );
+  }
+  
 
 export default App;
