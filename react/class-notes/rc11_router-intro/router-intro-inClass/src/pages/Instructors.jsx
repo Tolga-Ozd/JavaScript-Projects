c4fc8d9d25ed34d2053c414d9dd1e4c2854b2e02
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-19 21:43:02 +0300

    Instructors added

--- react/class-notes/rc11_router-intro/router-intro-inClass/src/pages/Instructors.jsx
+++ react/class-notes/rc11_router-intro/router-intro-inClass/src/pages/Instructors.jsx
@@ -1,7 +1,15 @@
+import {Link} from "react-router-dom"
+
 const Instructors = () => {
   return (
     <div className="p-3">
       <h1>Instructors</h1>
+      <div className="d-flex flex-column">
+        {/* <Link to = {"/instructors/1"}>Instructors-1</Link> */}
+        <Link to = {"1"}>Instructors-1</Link>
+        <Link to = {"2"}>Instructors-2</Link>
+        <Link to = {"3"}>Instructors-3</Link>
+      </div>
     </div>
   );
 };
