042b1f424bcf3aa47cf8677d02514dccac73491b
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-20 13:19:17 +0300

    useNavigate added

--- react/class-notes/rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
+++ react/class-notes/rc11_router-intro/router-intro-inClass/src/components/Nav.jsx
@@ -1,22 +1,31 @@
-import {Link} from "react-router-dom"
-
+import {Link , NavLink} from "react-router-dom"
 
 const Nav = () => {
+
+  //? Link, NavLink ve Navigate componentleri declarative routing
+  //? gerceklestirmek icin kullanilir.
+  //? Ornegin: Link ve NavLink Sayfada gorulebilen, kullanciyla
+  //? bir etkilesim icerisinde bulunarak yonledirme yapilan bir
+  //? componentlerdir. (Nav v.b)
+
   return (
     <ul className="bg-warning">
+
       <li>
         <Link to="/">Home</Link>
       </li>
+
       <li>
-        {/* <NavLink style ={(isActive)=>({color: isActive && "red"})}>Instructors</NavLink> */}
+        <NavLink style ={({isActive})=>({color: isActive && "red"})}
+         to = "/instructors">Instructors
+        </NavLink>
         {/* navlink ile active class ı ekleniyor */}
-
-        {/* <NavLink to = "/instructors" >Instructors </NavLink> */}
-
       </li>
+
       <li>
-        <Link to="/contact">Contact</Link>
+        <NavLink to="/contact">Contact</NavLink>
       </li>
+
     </ul>
   );
 };
