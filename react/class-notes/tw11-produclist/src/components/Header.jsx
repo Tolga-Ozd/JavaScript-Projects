9261cca11eb9afa6331ea7018ec4317d43d3c8a4
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-22 00:01:41 +0300

    last

--- react/class-notes/tw11-produclist/src/components/Header.jsx
+++ react/class-notes/tw11-produclist/src/components/Header.jsx
@@ -1,16 +1,43 @@
 import Button from 'react-bootstrap/Button';
+import axios from "axios";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 
 const Header = () => {
+    const getCategories =()=>{
+        
+        const renkli =[
+            "secondary",
+            
+        ]
+
+        const [categories , setCategories] = useState();
+
+        const C_URL ="https://fakestoreapi.com/products/categories";
+        axios.get(C_URL).then((res)=>setCategories(res.data));
+    };
+
+    useEffect(()=>{
+        getCategories()
+    } , []);
+
+    
+
   return (
     <div>
       <h1> Product List</h1>
       <div>
       <Button variant="primary">ALL</Button>{' '}
-      <Button variant="secondary">ELECTRONİCS</Button>{' '}
-      <Button variant="success">JEWELLERY</Button>{' '}
-      <Button variant="warning">MEN'S CLOTHİNG</Button>{' '}
-      <Button variant="danger">WOMEN CLOTHİNG</Button>{' '}
+     
+
+        {categories.map((item)=>{
+            return(
+                <Button variant="secondary">{item.toUpperCase()}</Button>
+            
+            )
+        })}
+
       </div>
     </div>
   )
