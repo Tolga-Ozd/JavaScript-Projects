a0aff87355309f6b132c016004c8746ae8f54ea8
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-23 21:49:01 +0300

    name fixed

--- react/class-notes/rc02_props/src/App.js
+++ react/class-notes/rc02_props/src/App.js
@@ -16,7 +16,7 @@ function App() {
       />
 
       <Cat
-        name="Garfield"
+        name="Mimmy"
         img="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
       />
     </div>
