6f0d158a1f70c762c8f3e3e6f31b76cb95813672
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-24 09:17:21 +0300

    keyboard onCut added

--- react/class-notes/rc06_forms-other-hooks/src/components/2-keybordsClipboard/KeyboardClipboard.jsx
+++ react/class-notes/rc06_forms-other-hooks/src/components/2-keybordsClipboard/KeyboardClipboard.jsx
@@ -1,50 +1,61 @@
 import { useState } from "react"
 
-
 const KeyboardClipboard = () => {
-    const [content, setContent] = useState(" ")
+  const [content, setContent] = useState("")
 
-    const handleKeydown = (e) => {
-        if(e.keyCode >= 48 && e.keyCode <= 57) {
-            alert( "Please dont enter a number")
-            e.preventDefault()
-        }
+  const handleKeyDown = (e) => {
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+      alert("Please dont enter a number")
+      e.preventDefault()
     }
-    // burada rakam girilmesini engelledik aski kodlarını kullanarak!
+  }
 
-    const handlePaste = (e)=>{
-        console.log(e.target)
-        e.target.className =  "form-control border border-success border-4"
-    }
+  const handlePaste = (e) => {
+    console.log(e.target)
+    // e.target.style.border = "3px solid red"
+    e.target.className = "form-control border border-danger border-4"
+    e.target.style.fontSize = "20px"
+  }
 
+  const handleCopy = (e) => {
+    alert("Coping illegal")
+    e.preventDefault()
+  }
+
+  const handleCut = (e) => {
+    alert("Cut illegal")
+    e.preventDefault()
+  }
   return (
     <div className="container mt-4">
-        <h2 className="text-center">KeyboardClipboard</h2>
+      <h2 className="text-center">Keyboard-Clipboard</h2>
 
-        <input
-         type="text" 
-         className="form-control"
-         onChange={(e) => {
-            setContent(e.target.value)
-            e.target.value = e.target.value.toLocaleUpperCase()
+      <input
+        type="text"
+        className="form-control"
+        onChange={(e) => {
+          // e.target.value = e.target.value.toLocaleUpperCase()
+          setContent(e.target.value.toLocaleUpperCase())
         }}
+        onKeyDown={handleKeyDown}
+        value={content}
+      />
 
-            onKeyDown={handleKeydown}
-            value={content}
-        />
+      <div className="mt-4">
+        <h3>Copied Content</h3>
+        <p onCopy={handleCopy} onCut={handleCut}>
+          {content.toLocaleLowerCase()}
+        </p>
+      </div>
 
-         <div className="mt-4">
-            <h3>Copied Content</h3>
-            <p>{content.toLocaleLowerCase()}</p>
-         </div>
-
-         <textarea 
-         name="form-control" 
-         id="" cols="30" 
-         rows="10"
-         onPaste={handlePaste}
-         ></textarea>
-      
+      <textarea
+        className="form-control"
+        name=""
+        id=""
+        cols="30"
+        rows="10"
+        onPaste={handlePaste}
+      ></textarea>
     </div>
   )
 }
