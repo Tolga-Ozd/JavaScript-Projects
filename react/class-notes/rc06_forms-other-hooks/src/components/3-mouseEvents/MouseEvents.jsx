e9d75cdfb25cac44c26bc9be36a5a424f5635bb3
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-24 09:40:19 +0300

    setCoord

--- react/class-notes/rc06_forms-other-hooks/src/components/3-mouseEvents/MouseEvents.jsx
+++ react/class-notes/rc06_forms-other-hooks/src/components/3-mouseEvents/MouseEvents.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react"
 
 const MouseEvent = () => {
-  const [visible, setVisible] = useState(false)
+  
+  const [visible, setVisible] = useState(false)  
 
   const [coordX, setCoordX] = useState()
   const [coordY, setCoordY] = useState()
@@ -32,6 +33,9 @@ const MouseEvent = () => {
 
       {/* Conditional Rendering */}
       {visible && <div>Hint</div>}
+      {/* visible true ise hinti göster demek */}
+
+
 
       <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
         todo item 2
