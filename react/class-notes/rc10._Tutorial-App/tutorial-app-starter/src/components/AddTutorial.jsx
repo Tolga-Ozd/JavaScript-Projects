478455ee2a2650e70d04edd5a336f4f1e00b6d50
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-25 15:53:36 +0300

    add tutorial

--- react/class-notes/rc10._Tutorial-App/tutorial-app-starter/src/components/AddTutorial.jsx
+++ react/class-notes/rc10._Tutorial-App/tutorial-app-starter/src/components/AddTutorial.jsx
@@ -6,6 +6,8 @@ const AddTutorial = () => {
 
   const handleSubmit = (e) => {}
 
+  
+
   return (
     <div className="container text-center mt-4">
       <h1 className="display-6 text-danger">Add Your Tutorial</h1>
