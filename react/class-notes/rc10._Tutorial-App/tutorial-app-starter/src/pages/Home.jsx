3552bcefce751f7808cc7ff7e536db5e48835b70
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-25 17:20:07 +0300

    tutorial added

--- react/class-notes/rc10._Tutorial-App/tutorial-app-starter/src/pages/Home.jsx
+++ react/class-notes/rc10._Tutorial-App/tutorial-app-starter/src/pages/Home.jsx
@@ -1,11 +1,36 @@
+import { useEffect, useState } from "react";
 import AddTutorial from "../components/AddTutorial";
 import TutorialList from "../components/TutorialList";
+import axios from "axios"
 
 const Home = () => {
+
+const [tutorials , setTutorials] = useState([])
+
+
+
+const getTutorials = async () => {
+  const BASE_URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+try {
+  const {data} = await axios(BASE_URL)
+  setTutorials(data) 
+  
+} catch (error) {
+  console.log(error)
+}
+}
+
+console.log(tutorials)
+
+// !mount aşamasında api'ye istek atıyoruz
+useEffect(()=>{
+  getTutorials()
+}, [])
+
   return (
     <>
-      <AddTutorial />
-      <TutorialList />
+      <AddTutorial getTutorials={getTutorials}/>
+      <TutorialList tutorials={tutorials} />
     </>
   );
 };
