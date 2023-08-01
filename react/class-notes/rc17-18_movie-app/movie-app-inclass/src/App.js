5e87b536c35088c02b6387cd05868bc83b460a35
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-01 11:56:29 +0300

    fixed

--- react/class-notes/rc17-18_movie-app/movie-app-inclass/src/App.js
+++ react/class-notes/rc17-18_movie-app/movie-app-inclass/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
+// import AppRouter from "../router/AppRouter";
 
 const App = () => {
   return (
-    <div>
+    <div className="gray-dark-main min-h-screen">
+      {/* <AppRouter /> */}
       <Navbar />
       <Register />
     </div>
