b8e11fa0e7a6cf192e24da68989de0400aafd74e
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-04 16:20:42 +0300

    add typo

--- react/class-notes/rc19_mui-intro/mui-intro-vite/src/src/components/Typo.jsx
+++ react/class-notes/rc19_mui-intro/mui-intro-vite/src/src/components/Typo.jsx
@@ -1,10 +1,29 @@
 import Typography from "@mui/material/Typography" ;
+import Box from "@mui/material/Box"
+import { Container } from "@mui/material";
 
 const Typo = () => {
   return (
-    <div>
-        <Typography> Mui Typo </Typography >
-    </div>
+    <Container maxWidth= "sm">
+         <Box sx={{
+        padding:"1rem" ,
+        display :"flex",
+        flexDirection:"column",
+        }}>
+        <Typography variant="h6" component= "h3" color={"lightcoral"} mt={4}> Mui Typo </Typography >
+
+        <Typography variant="subtitle3" component= "h1" color={"lightcoral"}> Mui Typo </Typography >
+
+            {/* span elementi */}
+        <Typography variant="button"  color={"yellow"} >Button Text </Typography >
+
+        {/* p elementi */}
+        <Typography variant="body2"  sx= {{color: "blue" , backgroundColor :"red" , mt:"1rem"}} align="center">Button Text </Typography >
+
+    </Box>
+
+    </Container>
+   
   )
 }
 
