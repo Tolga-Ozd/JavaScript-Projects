2f0317ca7befc8c98ee688132f62752082011bb1
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-13 12:55:03 +0300

    todoReducer added

--- react/class-notes/redux-intro-inClass/src/Store/todoReducer.js
+++ react/class-notes/redux-intro-inClass/src/Store/todoReducer.js
@@ -1,21 +1,31 @@
 // rxreducer kısayolu
 
+//todo reducer type todo 
 export const ADD_TODO = "ADD_TODO"
 export const DELETE_TODO = "DELETE_TODO"
-export const DELETE_TODO = "DELETE_TODO"
+export const TOGGLE_TODO = "TOGGLE_TODO"
+export const CLEAR_TODO = "CLEAR_TODO"
 
 
+//action function declaration
+export  const addTodo = (payload) = ({type: ADD_TODO , payload })
 
 //?başlangıç durumları
 const initialState = {
-    todoList: [{id: new Date() , text: "work redux" , completed: false} , ]
+    todoList: [{id: new Date() , text: "work redux" , completed: false} ,
+    {},
+  ],
 }
 
-export default (state = initialState, { type, payload }) => {
+export const todoReducer= (state = initialState, { type, payload }) => {
   switch (type) {
-
-  case typeName:
-    return { ...state, ...payload }
+  case ADD_TODO:
+    return {
+      todoList : [
+        ...state.todoList, 
+        {id:newDate().getTime() , text: payload, completed: false },
+      ],
+    }
 
   default:
     return state
