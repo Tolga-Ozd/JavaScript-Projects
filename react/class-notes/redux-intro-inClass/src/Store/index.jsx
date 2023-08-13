02705fa9e3feef6ebdb9ffafabe3c281a679aaa2
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-08-13 13:01:50 +0300

    combine reducer added

--- react/class-notes/redux-intro-inClass/src/Store/index.jsx
+++ react/class-notes/redux-intro-inClass/src/Store/index.jsx
@@ -1,6 +1,14 @@
-import {createStore} from "redux"
+import {createStore , combineReducers} from "redux"
 import { counterReducer } from "./counterReducer"
+import { todoReducer } from "./todoReducer"
 
 // ! store u oluşturduk
-export const store = createStore(counterReducer)
+// export const store = createStore(counterReducer)
+
+const rootReducer = combineReducers({
+    counterReducer,
+    todoReducer,
+})
+
+export const store = createStore(rootReducer)
 
